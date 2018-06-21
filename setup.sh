71d565a7e13e821d5726277383d66b923034ca69
Jordão Macedo <jordao05@hotmail.com>
2018-06-21 13:12:06 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt-get update
 
-sudo apt-get install php php-fpm php-cli php-common php-mbstring php-gd php-intl php-xml php-mysql php-mcrypt php-zip -y --allow-unauthenticated
+sudo apt-get install php php7.0-fpm php7.0-cli php7.0-common php7.0-mbstring php7.0-gd php7.0-intl php7.0-xml php7.0-mysql php7.0-mcrypt php7.0-zip -y --allow-unauthenticated
 sudo apt-get install libapache2-mod-php -y --allow-unauthenticated
 sudo service apache2 restart
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
